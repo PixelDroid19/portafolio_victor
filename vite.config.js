44d8475d1e3fb7feac7d993c4df67d3e04175cb0
Damien Monasterios <86851423+PixelDroid19@users.noreply.github.com>
2023-05-16 00:23:51 -0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://PixelDroid19.github.io/PortfolioVictor',
+  base: "https://PixelDroid19.github.io/portafolio_victor",
   server: {
-    host: true
-  }
-})
+    host: true,
+  },
+});
